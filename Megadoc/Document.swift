d6a4d2c1d1a3f525b0be9936cfc332b07274dfd7
Paco Gorina <fgorina@gmail.com>
2017-11-14 10:40:22 +0100

    Initial Commit

---
+++ Megadoc/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  Megadoc
+//
+//  Created by Francisco Gorina Vanrell on 14/11/17.
+//  Copyright © 2017 Francisco Gorina. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
